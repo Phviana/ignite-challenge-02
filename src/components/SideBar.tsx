432743eb279a842483c898fc403ed5bc09d12254
Paulo Viana Silva <paulo.silva@safefood360.com>
2021-11-23 13:39:27 +0000

    challenge finalized :fire:

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import {useState} from 'react';
+
+interface SidebarProps{
+  genres: GenreResponseProps[];
+  handleClickButton: Function;
+  selectedGenreId: number;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SidebarProps) {
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
